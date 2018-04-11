29d19813d4889dd8732a4f8f3e797d9ff0a0306f
Kara Cavanaugh <karacav@msn.com>
2018-04-11 00:01:03 -0400

    divisible

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 //Hello World
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World");
+//     }
+// }
+
+//Create a function isDivisible(n, x, y) that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public static Boolean main(long n, long x, long y) {
+        return (n % x == 0) && (n % y == 0);
     }
 }
